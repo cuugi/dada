88d9df12a22751324a335c438ee02ee44997c681
Jukka Loukkaanhuhta <jukka.loukkaanhuhta@polar.fi>
2014-01-02 08:29:02 +0200

    Added figures (calories, distance and heart rate average) to JsonOutput.

--- src/main/scala/dada/output/JsonOutput.scala
+++ src/main/scala/dada/output/JsonOutput.scala
@@ -1,9 +1,12 @@
 package dada.output
 
-import dada.{Sample, Dimension, Activity}
+import dada._
 import scala.util.parsing.json.{JSONFormat, JSONArray, JSONObject}
 import org.joda.time.{DateTimeZone, Duration, DateTime}
 import Dimension._
+import scala.util.parsing.json.JSONArray
+import scala.util.parsing.json.JSONObject
+import dada.Dimension
 
 class JsonOutput(activity: Activity) extends Output {
   require(activity != null)
@@ -13,6 +16,8 @@ class JsonOutput(activity: Activity) extends Output {
   private def toValue(value: Any): Any =
     value match {
       case null => "null"
+      case o: Option[_] => toValue(o.get)
+      case f: Figure[_] => toValue(f.value)
       case d: DateTime => d.toDateTime(DateTimeZone.UTC)
       case n: Number => n
       case _ => value.toString
@@ -23,6 +28,13 @@ class JsonOutput(activity: Activity) extends Output {
       "interval" -> toValue(interval),
       "values" -> new JSONArray(activity.getIntervalledSamples(dimension, interval, activity.duration).map(toValue))
     ))
+
+  private def statistics(dimension: Dimension): JSONObject =
+    new JSONObject(Map(
+      "minimum" -> activity.getFigure(dimension, FigureType.Minimum),
+      "average" -> activity.getFigure(dimension, FigureType.Average),
+      "maximum" -> activity.getFigure(dimension, FigureType.Maximum)
+    ).filter(_._2.isDefined).mapValues(_.get.value))
 
   private def route: JSONObject = {
     val sorted = activity.sorted
@@ -46,7 +58,12 @@ class JsonOutput(activity: Activity) extends Output {
     new JSONObject(Map(
       "startTime" -> toValue(activity.id.toLocalDateTime),
       "stopTime" -> toValue(activity.stopTime.toLocalDateTime),
+      "distance" -> toValue(activity.getFigure(Dimension.Distance, FigureType.Exact)),
+      "kiloCalories" -> toValue(activity.getFigure(Dimension.Energy, FigureType.Exact)),
       "duration" -> toValue(activity.duration),
+      "statistics" -> new JSONObject(Map(
+        "heartRate" -> statistics(Dimension.HeartRate)
+      )),
       "samples" -> new JSONObject(Map(
         "distance" -> samples(Dimension.Distance, sampleInterval),
         "speed" -> samples(Dimension.Speed, sampleInterval),
